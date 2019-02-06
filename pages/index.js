36ccaeb6cb43f93404bc99b353b953f8e825194a
Kristian Trandem <kkrorkk@gmail.com>
2019-02-06 13:04:07 +0100

    start using react

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default () => <div>Welcome</div>;
+import React from "react";
+
+const Index = () => (
+  <div>
+    <h1>Welcome</h1>
+    <p>This is text</p>
+    <img src="http://images5.fanpop.com/image/photos/31000000/haters-gonna-hate-random-31076705-550-413.jpg" />
+    <p>This is image</p>
+  </div>
+);
+
+export default Index;
