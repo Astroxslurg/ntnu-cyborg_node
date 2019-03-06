65dc5ad223fa86aa98ea844d0c714b733683d3a0
Kristian Trandem <kristiantrandem@gmail.com>
2019-03-06 14:54:37 +0100

    play around with canvas

--- components/visualisation.js
+++ components/visualisation.js
@@ -19,28 +19,44 @@ class Visualisation extends Component {
     const ctx = canvas.getContext('2d');
 
     // Create gradient
-    var grd = ctx.createLinearGradient(0, 0, 500, 0);
-    grd.addColorStop(0, 'red');
-    grd.addColorStop(1, 'white');
+    // var grd = ctx.createLinearGradient(0, 0, 500, 0);
+    // grd.addColorStop(0, 'red');
+    // grd.addColorStop(1, 'white');
+
+    const radius = this.state.canvasSize / 2;
+    const startCords = [radius, radius];
+    const endCords = [radius, radius];
+    const endRadius = radius;
+
+    const gradient = ctx.createRadialGradient(
+      startCords[0],
+      startCords[1],
+      0,
+      endCords[0],
+      endCords[1],
+      endRadius
+    );
+    gradient.addColorStop(0, 'red');
+    gradient.addColorStop(1, 'white');
 
     // Fill with gradient
-    ctx.fillStyle = grd;
+    ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, 500, 500);
 
-    ctx.lineWidth = 16;
+    ctx.lineWidth = 6;
     ctx.lineCap = 'round';
 
-    ctx.beginPath();
-    ctx.arc(250, 250, 200, 0.3, Math.PI - 0.3);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.arc(250, 250, 200, 0.3, Math.PI - 0.3);
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.ellipse(150, 160, 25, 55, 0, 0, 2 * Math.PI);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.ellipse(150, 160, 25, 55, 0, 0, 2 * Math.PI);
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.ellipse(350, 160, 25, 55, 0, 0, 2 * Math.PI);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.ellipse(350, 160, 25, 55, 0, 0, 2 * Math.PI);
+    // ctx.stroke();
   }
 
   render() {
