ad76c56a8ee2937571ea7ce3e348cf68e461738a
Herman Stavelin <staveliney@gmail.com>
2019-03-06 15:04:51 +0100

    implement the plots into the carousel and display it in the entry page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import dynamic from 'next/dynamic'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+const onChange = () => true;
+const onClickItem = () => true;
+const onClickThumb = () => true;
 
 const DynamicPlot = dynamic(import('../components/plot'), {
     ssr: false
 })
 
 const Index = ({ state }) => (
-  <div>
-    <DynamicPlot/>
+    <div>
+    <Carousel
+      showArrows={true}
+      //emulateTouch
+      //width="400"
+      //onChange={onChange}
+      centerMode={true}
+      intiniteLoop={true}
+      //onClickThumb={onClickThumb}
+      //onClickItem={onClickItem}>
+        >
+      <div>
+          <DynamicPlot/>
+        <p className="legend">yoyo</p>
+      </div>
+      <div>
+        <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+        <p className="legend">sup?</p>
+      </div>
+      <div>
+        <div
+          style={{
+            display: 'inline-block',
+            pointerEvents: 'none',
+            backgroundColor: '#ff0044',
+          }}>
+          <iframe src="/apitest" />
+        </div>
+        <p className="legend">an iframe to /apitest</p>
+      </div>
+    </Carousel>
   </div>
 );
 
