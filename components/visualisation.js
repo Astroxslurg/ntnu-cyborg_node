c15180202aaa7bfb2b227c823ad3c8bb02452177
Kristian Trandem <kristiantrandem@gmail.com>
2019-03-27 09:39:22 +0100

    make canvas visualisation work in general

--- components/visualisation.js
+++ components/visualisation.js
@@ -2,24 +2,47 @@ import React, { Component } from 'react';
 import Test from '../components/test';
 import Api from '../src/api-client';
 
-const getGradient = currentColor => {
-  const r = parseInt(currentColor[1].concat(currentColor[2]));
-  const g = parseInt(currentColor[3].concat(currentColor[4]));
-  const b = parseInt(currentColor[5].concat(currentColor[6]));
+const getGradient = value => {
+  if (value < 0) {
+    console.log(`Error: getGradient got negative value`);
+    return '#000000';
+  }
+  const modVal = value % 768;
+  let r, g, b;
+  if (modVal < 256) {
+    r = 255 - modVal;
+    g = modVal;
+    b = 0;
+  } else if (modVal < 512) {
+    r = 0;
+    g = 256 - (modVal % 256);
+    b = modVal % 256;
+  } else {
+    r = modVal % 256;
+    g = 0;
+    b = 256 - (modVal % 256);
+  }
+  return String.prototype.concat(
+    '#',
+    r.toString(16).padStart(2, '0'),
+    g.toString(16).padStart(2, '0'),
+    b.toString(16).padStart(2, '0'),
+  );
 };
 
-const draw = (arr, index, color, seqOfSameColor, seq) => {
-  color = (parseInt(color[1].concat(color[2]), 16) + 15).toString(16);
-  ctx.strokeStyle = color;
-
-  const reverter = Math.PI ? dataList[i] < 0 : 0;
+const draw = (arr, index, gradientValue, ctx, multiplier) => {
+  const strokeString = getGradient(gradientValue);
+  ctx.strokeStyle = strokeString;
+  // console.log(`gradientValue: ${gradientValue}, strokeString: ${strokeString}`);
+  // console.log(`val: ${arr[index]}`);
+  const reverter = arr[index] < 0 ? Math.PI : 0;
   ctx.beginPath();
   ctx.arc(
     250,
     250,
-    Math.abs(dataList[i]) * multiplier,
-    i * increment + reverter,
-    (i + 1) * increment + reverter
+    Math.abs(arr[index]) * multiplier,
+    0 + reverter,
+    Math.PI + reverter,
   );
   ctx.stroke();
 };
@@ -34,6 +57,8 @@ class Visualisation extends Component {
   }
 
   async componentDidMount() {
+    // const result = await Api.fetchNodesForTimeInterval([12, 21], 0, 1000);
+    console.log(result);
     const result = await Api.fetchSensorData();
     this.setState({ sensordata: result });
 
@@ -41,73 +66,35 @@ class Visualisation extends Component {
     const ctx = canvas.getContext('2d');
 
     const size = this.state.canvasSize;
-    // const color = '#72c036';
-    // const otherColor = '#c92e67';
-    // const color = '#00d026';
-    // const otherColor = '#d90097';
     const dataList = this.state.sensordata.resultList;
+
     const increment = (2 * Math.PI) / dataList.length;
-    const seqOfSameColor = Math.floor(dataList.length / 768);
+    const seqOfSameColor =
+      dataList.length < 768 ? 1 : Math.floor(dataList.length / 768);
 
     const radius = size / 2;
 
     const maxVal = Math.max(
       Math.abs(Math.min(...dataList)),
-      Math.max(...dataList)
+      Math.max(...dataList),
     );
     const multiplier = radius / maxVal;
 
     const startCords = [radius, radius];
     const endCords = [radius, radius];
     const endRadius = 120;
-    //
-    // const gradient = ctx.createRadialGradient(
-    //   startCords[0],
-    //   startCords[1],
-    //   0,
-    //   endCords[0],
-    //   endCords[1],
-    //   endRadius
-    // );
-    // gradient.addColorStop(1, color);
-    // gradient.addColorStop(0, otherColor);
-
-    // Fill with gradient
-    // ctx.fillStyle = gradient;
-    // ctx.fillRect(0, 0, size, size);
 
     ctx.lineWidth = 6;
     ctx.lineCap = 'round';
-    // ctx.strokeStyle = color;
 
-    // console.log(increment);
-    // console.log(dataList);
-
-    let color = '#000000';
+    let seq = -seqOfSameColor;
+    let gradientValue = 0;
 
     for (let i = 0; i < dataList.length; i++) {
-      color = (parseInt(color[1].concat(color[2]), 16) + 15).toString(16);
-      ctx.strokeStyle = color;
-
-      const reverter = Math.PI ? dataList[i] < 0 : 0;
-      ctx.beginPath();
-      ctx.arc(
-        250,
-        250,
-        Math.abs(dataList[i]) * multiplier,
-        i * increment + reverter,
-        (i + 1) * increment + reverter
-      );
-      ctx.stroke();
+      seq = (seq + 1) % seqOfSameColor;
+      gradientValue = seq ? gradientValue : gradientValue + 24;
+      draw(dataList, i, gradientValue, ctx, multiplier);
     }
-
-    // ctx.beginPath();
-    // ctx.ellipse(150, 160, 25, 55, 0, 0, 2 * Math.PI);
-    // ctx.stroke();
-
-    // ctx.beginPath();
-    // ctx.ellipse(350, 160, 25, 55, 0, 0, 2 * Math.PI);
-    // ctx.stroke();
   }
 
   render() {
